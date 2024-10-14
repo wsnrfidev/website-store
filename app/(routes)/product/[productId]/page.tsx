7ea035313f3da50059a0ff19a1020f6a2311f0a2
wsnrfidev <bisabismillah04@gmail.com>
2024-10-15 01:38:55 +0700

    update

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -14,30 +14,25 @@ interface ProductPageProps {
 const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
   const product = await getProduct(params.productId);
 
-
   const suggestedProducts = await getProducts({
     categoryId: product?.category?.id,
   });
-
   return (
     <div className="bg-white">
-        <Container>
-            <div className="px-4 py-10 sm:px-6 lg:px-8">
-             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                <Gallery images={product.images} />
-                <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                    <Info data={product}/>
-                </div>
-             </div>
-             <hr className="my-10"/>
-             <ProductList 
-             title="Product Terkait"
-             items={suggestedProducts}
-             />    
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <Gallery images={product.images} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+            <Info data={product} />
             </div>
-        </Container>
+          </div>
+          <hr className="my-10" />
+          <ProductList title="Produk Terkait" items={suggestedProducts} />
+        </div>
+      </Container>
     </div>
-  )
+  );
 };
 
 export default ProductPage;
